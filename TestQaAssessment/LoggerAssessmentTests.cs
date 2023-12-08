2e056a43da07f2222c2404dac948e64b03a44c73
Almeidrade <andrade.clayton@hotmail.com>
2023-12-08 13:29:16 -0300

    Added some ajustments to cover more scenarios validations

--- TestQaAssessment/LoggerAssessmentTests.cs
+++ TestQaAssessment/LoggerAssessmentTests.cs
@@ -21,35 +21,58 @@ public class LoggerAssessmentTests
         dateTime = DateTime.Now.ToLongTimeString();
         date = DateTime.Now.ToLongDateString();
         lines = new List<string>(logger.readsTheLogFile(directory, "Log.txt"));
-        lastRecordId = lines.ElementAt(lines.Count-4);
+        // lastRecordId = lines.ElementAt(lines.Count-4);
+        lastRecordId = "c8797d8c-8fc5-4ee4-a2e8-ab631f45f7b0";
     }
 
     [Test]
     public void TestLastRecordPresenceInLastPosition()
     {
-        bool lastLogPresent = false;
-        bool lastLogInTheLastPosition = false;
+        bool lastLogStillPresent = false;
+        bool newLogRightPosition = false;
+        bool newLogInTheLastPosition = false;
+
         logLevel = LogLevel.INFO;
+        logger.Log(guid, "user accessed the system session - INFO", logLevel, loggedUser.Name, dateTime, date);
+        string newRecordID = guid.ToString();
+
+        lines = new List<string>(logger.readsTheLogFile(directory, "Log.txt"));
+
         for (int i = 0; i < lines.Count; i++)
         {
             if (lines[i].StartsWith("Log Entry") && lines[i].Contains(lastRecordId))
             {
-                lastLogPresent = true;
+                lastLogStillPresent = true;
+                
+                //if the 5th position below the `lastRecordId` is equals the `newRecordID`, it means that the record is correspondingly in the right position of the
+                //log file (right after the `lastRecordId`)
+                if (lines[i+5].Replace("Log Entry: ","") == newRecordID)
+                {
+                    newLogRightPosition = true;
 
-                //if the 5th position from the `Log Entry` line is null, it means that the record is correspondingly in the last position of the log file,
-                //once the return of the try block is going to be a catch (due to be null), otherwise it`s going to return the `Log Entry` of the next record
-                try 
-                {
-                    var aux = lines[i + 5];
+                    //if the 10th position below the `lastRecordId` line is null, it means that the record is correspondingly in the last position of the log file, once 
+                    //the return of the try block is going to be an exception (due to be null), otherwise it`s going to return the `Log Entry` of the next record present
+                    try 
+                    {
+                        if (lines[i + 10] != null) 
+                        {
+                           Assert.Fail("New log has been inserted right bellow the last record accordingly in the log file, but it was not the last one :()"); 
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        newLogInTheLastPosition = true; 
+                    }
                 }
-                catch (ArgumentOutOfRangeException e)
+                else
                 {
-                   lastLogInTheLastPosition = true; 
+                    Assert.Fail("New log was not inserted right after the last record or it's not present");
                 }
             }
         }
-        Assert.True(lastLogPresent);
-        Assert.True(lastLogInTheLastPosition);
+        Assert.True(lastLogStillPresent);
+        Assert.True(newLogRightPosition);
+        Assert.True(newLogInTheLastPosition);
     }
 
     [Test]
